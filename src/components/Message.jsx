aa9f73f6e25861aed514611d1d8864cb4e520fc6
Hrishikesh Bolinjkar <bolinjkar.rishi@gmail.com>
2021-12-09 20:45:42 +0530

    added delete button

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,15 +3,23 @@ import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import styled from "styled-components";
 import { auth } from "../config/fbconfig";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { IconButton } from "@mui/material";
 
 const Message = ({ message: { message, user, timestamp } }) => {
   const [userLoggedIn] = useAuthState(auth);
   const MessageType = user === userLoggedIn.phoneNumber ? Sender : Receiver;
+
   return (
     <Container>
       <MessageType>
         {message}
         <Timestamp>{moment(timestamp).format("LT")}</Timestamp>
+        {MessageType === Sender && (
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+        )}
       </MessageType>
     </Container>
   );
@@ -31,6 +39,15 @@ const MessageElement = styled.p`
   display: flex;
   justify-content: space-between;
   font-size: 0.9rem;
+  position: relative;
+  > button {
+    padding: 0.05rem;
+    z-index: 999;
+    position: absolute;
+    top: 0.1rem;
+    right: 0.1rem;
+    display: none;
+  }
 `;
 
 const Sender = styled(MessageElement)`
@@ -49,4 +66,11 @@ const Timestamp = styled.div`
   text-align: right;
   padding-top: 0.5rem;
   margin-left: 1.2rem;
+
+  &:hover + button,
+  button:hover {
+    display: inline-block;
+    background-color: #dcf8c6;
+    cursor: pointer;
+  }
 `;
